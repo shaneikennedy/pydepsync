7dc5616fd464919251dd9aacb1b02c93c3355fa9
Shane Kennedy <shane.kennedy19@gmail.com>
2025-03-06 22:45:23 +0100

    Swap reqwest for ureq, optimize release build

--- src/engine/resolver.rs
+++ src/engine/resolver.rs
@@ -3,7 +3,6 @@ use std::io;
 use crate::dependency::Dependency;
 
 use log::{debug, warn};
-use reqwest::blocking;
 use serde_json::Value;
 
 #[derive(Clone)]
@@ -41,7 +40,7 @@ impl PackageResolver {
     // TODO make this a much better http client, retries, backoff, error handling
     fn resolve_on_index(self, dep: &Dependency, index: &str) -> Option<Dependency> {
         let url = format!("{}/{}/json", index, dep.name());
-        let response = blocking::get(url.as_str());
+        let response = ureq::get(url.as_str()).call();
         if response.is_err() {
             warn!(
                 "Problem resolving package {} on index {}.",
@@ -51,17 +50,7 @@ impl PackageResolver {
             debug!("Error {}", response.unwrap_err());
             return None;
         }
-        let json = response.unwrap().json();
-        if json.is_err() {
-            warn!(
-                "Problem resolving package {} on index {}.",
-                dep.name(),
-                index,
-            );
-            debug!("Error {}", json.unwrap_err());
-            return None;
-        }
-        let json: Value = json.unwrap();
+        let json: Value = response.unwrap().body_mut().read_json().unwrap();
         if let Some(version) = json
             .get("info")
             .and_then(|info| info.get("version"))
